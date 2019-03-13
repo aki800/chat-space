e3112b438fed17c3c8b65c11473d0eb1a2e3a1db
kahorno <yamadakaho@yamadakahonoMacBook-Pro.local>
2019-03-13 15:48:04 +0900

    Asynchronous-communication
    #WHAT
    デバッグとその他不要なものを削除
    インデントの見直し
    #WHY
    コードを見やすくするため

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,20 +1,19 @@
 $(document).on('turbolinks:load', function() {
-
   $(function(){
     function buildHTML(message){
       var addImage = (message.image.url !== null) ? `<img class="lower-message_image" src="${message.image.url}">"` : ''
-      console.log(message.image.url);
       var html =  `<p class="user__name">
-    ${message.user_name}
-    <span class="user__date" style="color: #999999;
-          font-size: 12px;
-          font-weight: lighter;">
-     ${message.time}
-    </span></p>
-
-      <p class="user__message">
-        ${message.body}</p>
-      ${addImage} `
+                    ${message.user_name}
+                      <span class="user__date" style="color: #999999;
+                        font-size: 12px;
+                        font-weight: lighter;">
+                          ${message.time}
+                      </span>
+                   </p>
+                   <p class="user__message">
+                     ${message.body}
+                   </p>
+                   ${addImage} `
       return html;
     }
     $('#new_message').on('submit', function(e){
@@ -39,7 +38,6 @@ $(document).on('turbolinks:load', function() {
       .fail(function(){
         alert('メッセージを入力してください');
       })
-
     })
   })
 });
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,6 +1,5 @@
 json.body @message.body
 json.image @message.image
 json.time @message.created_at.strftime("%Y/%m/%d %H:%M")
-json.user_id @message.user.id
 json.user_name @message.user.name
 
