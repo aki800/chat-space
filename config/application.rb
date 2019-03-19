567bd4806eb78e2b4ead9a973e0a3755d70912af
kahorno <yamadakaho@yamadakahonoMacBook-Pro.local>
2019-03-19 15:32:42 +0900

    devise edit2

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,10 @@
 require_relative 'boot'
 
 require 'rails/all'
+
+require 'devise'
+
+require 'yaml'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
